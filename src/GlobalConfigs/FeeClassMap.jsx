c4da008641fa255fe34f2de5cec3f2b55070a7ef
Anoop <anoopmonu02@gmail.com>
2024-05-19 20:18:23 +0530

    aaaa

--- src/GlobalConfigs/FeeClassMap.jsx
+++ src/GlobalConfigs/FeeClassMap.jsx
@@ -6,7 +6,7 @@ import {
     useMaterialReactTable,
   } from 'material-react-table';
   import { Paper, Stack, Divider, Box, Avatar, Typography, Grid, TextField, CssBaseline, FormControl, InputLabel, Select, MenuItem } from '@mui/material'
-  import { useForm, Controller } from 'react-hook-form';
+  import { useForm, Controller, useFieldArray } from 'react-hook-form';
   import MyButton from '../components/MyButton';
   import { red } from '@mui/material/colors';
   import SaveOutlined from '@mui/icons-material/SaveOutlined';
@@ -14,6 +14,9 @@ import {
   import Button from '@mui/material/Button';
 import { PrjSelectFields } from '../components/PrjSelectFields';
 import { getGradeData } from '../utils/globalconfigsjs';
+import { Edit as EditIcon, Delete as DeleteIcon } from '@mui/icons-material';
+import { IconButton } from '@mui/material';
+import { Link } from 'react-router-dom';
 import * as yup from 'yup';
 
 function FeeClassMap() {
@@ -23,8 +26,10 @@ function FeeClassMap() {
     const [gradeList, setGradeList] = useState([]);
     const [grades, setGrades] = useState(null);
     const [feeheads, setFeeheads] = useState([]);
+    const [feeAmounts, setFeeAmounts] = useState([]);
+    const [formData, setFormData] = useState({});
 
-    const [validationSchema, setValidationSchema] = useState(yup.object().shape({}));
+    
     const 
     {
         register, 
@@ -33,13 +38,20 @@ function FeeClassMap() {
         reset,
         control,
         formState:{errors, isSubmitting},
-    } = useForm();
+    } = useForm({});
     
-
+    const {
+        fields,
+        append,
+        remove
+      } = useFieldArray({
+        control
+      });
     const getData = () => {      
         const access_token = localStorage.getItem('access_token');
         const branchid = localStorage.getItem('branch');        
         getGradeData(access_token, branchid, setGradeList);
+
         
     }
   
@@ -54,6 +66,9 @@ function FeeClassMap() {
       }, [grades]);
 
     const getFeeClassMappingData = async(e) =>{
+        setError("")
+        reset();
+        //setFeeheads([]);        
         const selectedGrade = e.target.value
         console.log("selectedGrade:>>>> ", selectedGrade)
         setGrades(selectedGrade);
@@ -64,45 +79,113 @@ function FeeClassMap() {
             "grade": selectedGrade,
             "branch": localStorage.getItem('branch'),
             "academicYear": localStorage.getItem('sessionid'),
+            "filterdata":"1"
         }
         await customaxios.get('/api/v1/admin/fee-class-mapping/',{
             params: qryData,
+        },{
+
         })
         .then(response => {
-            setFeeheads(response.data);
-            //setGrades(selectedGrade);
+            setFeeheads(response.data.fee_data);
+            setMyData(response.data.table_data);
             console.log("grades", grades)
             console.log("fee heads", response.data);
         })
         .catch(error => {
         console.error('Error fetching fee heads:', error);
         });
-    }
-
-    useEffect(() => {
-        let schema = {};
-        feeheads.forEach(field => {
-            console.log("field", field);
-            schema[field.amount] = yup.number().required(`${field.amount} is required`);
-        // You can add more validation rules here as needed
-        });
-        setValidationSchema(yup.object().shape(schema));
-      }, [feeheads]);
+        setLoading(false);
+    }    
     
-      const saveFeeClassMapping = (data) => {
+      const saveFeeClassMapping = async(data) => {
         console.log("data", data)
         console.log("Grades", grades)
         setError("");
+        setLoading(true);
         const branch= localStorage.getItem('branch');
         const academicYear= localStorage.getItem('sessionid');
-        const transformedData = Object.entries(data).map(([feename, amount]) => ({
-            feename,
-            branch: branch, // Assuming branch and academicYear are constant
-            academicYear: academicYear, // Assuming branch and academicYear are constant
-            amount
-          }));  
-        console.log("-----------",transformedData);  
-    }
+        const fee_class_map_data = {
+            "branch": branch, // Assuming branch and academicYear are constant
+            "academicYear": academicYear, // Assuming branch and academicYear are constant
+            "grade": grades
+        };
+        let fee_class_amount_map_data = Object.entries(data).map(([feeheadid, amount]) => ({
+            /* if(feeheadid.startsWith("FeeHead_")) {
+                feeheadid = feeheadid.slice(8)
+            } */
+            "feehead" : feeheadid.startsWith("FeeHead_") && feeheadid!=='grade'?feeheadid.slice(8):'',            
+            amount,            
+          })); 
+        fee_class_amount_map_data = fee_class_amount_map_data.filter(item=>item.feehead.trim()!=='')          
+        console.log("-----------",fee_class_amount_map_data);  
+        const transformedData = {
+            "branch": branch, // Assuming branch and academicYear are constant
+            "academicYear": academicYear, // Assuming branch and academicYear are constant
+            "grade": grades,
+            "fee_class_map": fee_class_map_data,
+            "fee_class_amount_map_fee_class": fee_class_amount_map_data
+        };
+        try {
+            console.log("Transformed Data:", transformedData);
+            const res = await customaxios.post('/api/v1/admin/fee-class-mapping/', transformedData);
+            if(res){
+                alert('Fee Class Mapping saved successfully');
+                setMyData(res.data)
+            }
+            getData();
+            reset();
+            setLoading(false);
+        } catch (error) {
+            
+        }
+    }   
+
+      const handleAmountChange = (e) => {
+        const { name, value } = e.target;
+        setFeeAmounts(prevState => ({
+          ...prevState,
+          [name]: value
+        }));
+        setFormData({ ...formData, [name]: value });
+        console.log("formData", formData);
+        console.log("feeAmounts", feeAmounts);
+      };
+      const columns = useMemo(
+        () => [
+          /* {
+            accessorKey: 'id', //access nested data with dot notation
+            header: 'ID',
+            size: 100,
+          }, */
+          {
+            accessorKey: 'grade', //access nested data with dot notation
+            header: 'Grade',
+            size: 150,
+          },
+          {
+            accessorKey: 'fee_head', //access nested data with dot notation
+            header: 'Fee Name',
+            size: 250,
+          },
+          {
+            accessorKey: 'amount', //access nested data with dot notation
+            header: 'Amount',
+            size: 100,
+          },
+          /* {
+            accessorKey: 'academicYear_format', //access nested data with dot notation
+            header: 'Academic Year',
+            size: 100,
+          },
+          {
+            accessorKey: 'priority',
+            header: 'Priority',
+            size: 100,
+          }, */
+        ],
+        [],
+      );
 
   return (
     <>
@@ -139,31 +222,30 @@ function FeeClassMap() {
                             <tbody>
                             {feeheads.map(feeHead => (
                                 <tr key={feeHead.feehead}>
-                                <td style={{ border: '1px solid #ddd', padding: '8px', textAlign: 'left' }}>{feeHead.feename}</td>
+                                <td style={{ border: '1px solid #ddd', padding: '8px', textAlign: 'left' }}>{feeHead.feename? feeHead.feename: feeHead.head_name}</td>
                                 <td style={{ border: '1px solid #ddd', padding: '8px', textAlign: 'left' }}>
-                                    {/* <input type="number" value={feeHead?.amount} onChange={(e) => handleAmountChange(e, feeHead.feehead)} /> */}
-
-                                    <Controller
-                                            name={feeHead.feename}
-                                            control={control}
-                                            render={({ field }) => (
-                                                <>
-                                                    <input
-                                                        type="number"
-                                                        value={feeHead?.amount}
-                                                        onChange={(e) => handleAmountChange(e, feeHead.feehead)}
-                                                        {...field}
-                                                        style={{ width: '60px', padding: '5px', borderRadius: '4px', border: '1px solid #ccc' }}
-                                                        min={0}
-                                                        disabled={isSubmitting}
-                                                    />
-                                                    {errors[feeHead.feename] && <p style={{ position: 'absolute', bottom: '-5px', color: 'red', margin: 0, fontSize: '12px' }}>{errors[feeHead.feename].message}</p>}
-                                                </>                                                
-                                            )}
+                                   {/*  {feeHead.head_name && feeHead.amount>0 && feeHead.amount !== undefined && feeHead.amount !== null ?(
+                                        <input type='number' name={feeHead.feehead} {...register(`${feeHead.feehead}`)} min={0} onChange={handleAmountChange}  disabled={isSubmitting}                                     
+                                        value={feeHead.amount}/>
+                                    ):(
+                                        <input type='number' name={feeHead.feehead} {...register(`${feeHead.feehead}`)} min={0} onChange={handleAmountChange}  disabled={isSubmitting}
                                         />
-                                        <p style={{ margin: 0, height: '20px' }}>&nbsp;</p>
-
-
+                                    )} */}
+                                    <input
+                                        type="number"
+                                        id={feeHead.feehead}
+                                        {...register(`FeeHead_${feeHead.feehead}`)}
+                                        defaultValue={feeHead.amount ? feeHead.amount : ''}
+                                        min={0}
+                                        disabled={isSubmitting}
+                                        style={{
+                                            width: '100px',  // Example width
+                                            padding: '8px',  // Example padding
+                                            border: '2px solid #ccc',  // Example border
+                                            borderRadius: '4px',  // Example border radius
+                                            // Add more CSS properties as needed
+                                        }}
+                                    />
 
                                 </td>
                                 </tr>
